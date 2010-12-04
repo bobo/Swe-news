b1e5042bc29a7a309ad086c63f6f3e4ae5ddffa5
Mikael Sundberg <mikael.sundberg82@gmail.com>
2010-12-04 20:41:38 +0100

    updated project file

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject Swenews "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :namespaces [Swenews.app_servlet]
-    :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [enlive  "1.0.0-SNAPSHOT"]
                  [net.cgrand/moustache "1.0.0-SNAPSHOT"]
                  [ring/ring-core "0.2.5"]
                  [ring/ring-devel "0.2.5"]
                  [joda-time "1.6"]
-                 [ring/ring-jetty-adapter "0.2.5"]]
-    :dev-dependencies [[appengine-magic "0.3.0-SNAPSHOT"]])
+                 [ring/ring-jetty-adapter "0.2.5"]
+                 [redis-clojure "1.0.4"]
+                 [congomongo "0.1.3-SNAPSHOT"]]
+    )
