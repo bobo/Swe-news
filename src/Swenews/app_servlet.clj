fd42efc5ee102cf969db9ce24b03650a244b770b
Mikael Sundberg <mikael.sundberg82@gmail.com>
2010-12-04 20:27:17 +0100

    initial non gae version

--- src/Swenews/app_servlet.clj
+++
@@ -1,8 +0,0 @@
-(ns Swenews.app_servlet
-  (:gen-class :extends javax.servlet.http.HttpServlet)
-  (:use Swenews.core)
-  (:use [appengine-magic.servlet :only [make-servlet-service-method]]))
-
-
-(defn -service [this request response]
-  ((make-servlet-service-method Swenews-app) this request response))
